a9502f870dc1ce4ef6583f34c317ce56fc40bea5
goosetech <30989867+goosetech@users.noreply.github.com>
2017-08-14 13:17:57 -0400

    Update capture.py

--- Acceleroma/scripts/capture.py
+++ Acceleroma/scripts/capture.py
@@ -6,9 +6,10 @@ import datetime
 from time import sleep
 from subprocess import call 
 
-
-call('fswebcam -r 1920x1080 --no-banner ~/Projects/GooseTech/Accelerom/images/%Y-%m-%d_%H:%M:%S.jpg', shell=True)
+for i in range(5):
+    call('fswebcam -r 1920x1080 --no-banner ~/Projects/GooseTech/Accelerom/images/%Y-%m-%d_%H:%M:%S.jpg', shell=True)
 call('git add -A', shell=True)
 call('git commit -m "Uploading image"', shell=True)
 call('git push acceleroma master', shell=True)
+
 

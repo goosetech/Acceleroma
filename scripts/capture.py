da510940bcd3133193a56781ef7a3e727d26fa43
goosetech <30989867+goosetech@users.noreply.github.com>
2017-08-13 21:15:13 -0400

    Add files via upload
    sample layout

---
+++ scripts/capture.py
@@ -0,0 +1,14 @@
+
+
+
+import os
+import datetime
+from time import sleep
+from subprocess import call 
+
+
+call('fswebcam -r 1920x1080 --no-banner /myProj/%Y-%m-%d_%H:%M:%S.jpg', shell=True)
+#call('git add -A', shell=True)
+#call('git commit -m "PI: Taking and uploading photo"', shell=True)
+#call('git push origin master', shell=True)
+

1c61bf35fd736acbaeea2193043103c0b1d841e8
goosetech <30989867+goosetech@users.noreply.github.com>
2017-08-13 21:17:55 -0400

    Add files via upload

--- scripts/capture.py
+++ scripts/capture.py
@@ -7,7 +7,7 @@ from time import sleep
 from subprocess import call 
 
 
-call('fswebcam -r 1920x1080 --no-banner /myProj/%Y-%m-%d_%H:%M:%S.jpg', shell=True)
+call('fswebcam -r 1920x1080 --no-banner /images/%Y-%m-%d_%H:%M:%S.jpg', shell=True)
 #call('git add -A', shell=True)
 #call('git commit -m "PI: Taking and uploading photo"', shell=True)
 #call('git push origin master', shell=True)
